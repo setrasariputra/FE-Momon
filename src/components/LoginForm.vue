58a7e826affc0e2a1bc7ac95cdef242b1b432dd0
Wisnu K. Ardianto <setrasariputra@gmail.com>
2023-04-02 00:00:58 +0700

    Update Router

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,13 +1,4 @@
 <template>
-  <h1>{{ title }}</h1>
-  <h2>{{ inputemail }}</h2>
-  <h3>{{  inputpwd }}</h3>
-  <h4 v-if="active === false">Tidak Aktif</h4>
-  <h4 v-else>Active</h4>
-  <p v-if="old <= 25">Remaja</p>
-  <p v-else-if="(old > 25) && (old <= 40)">Dewasa</p>
-  <p v-else="old > 40">Orang Tua</p>
-
   <!-- Start Block Logo-->
   <img src="../assets/logo/momon.svg" class="mon-login-logo" />
   <!-- End Block Logo-->
