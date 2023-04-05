62d38ef373d1d2c590176a1f131dd41309285526
Wisnu K. Ardianto <setrasariputra@gmail.com>
2023-04-05 23:58:27 +0700

    Proses Login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -9,7 +9,7 @@
       <div class="mon-colinput">
         <label>Email</label>
         <input
-          v-model="inputemail"
+          v-model="form.email"
           type="email"
           name="email"
           class="forminput"
@@ -22,25 +22,64 @@
       <img src="../assets/icon/kunci.svg" class="input-icon" />
       <div class="mon-colinput">
         <label>Password</label>
-        <input v-model="inputpwd" type="password" name="password" placeholder="******" class="forminput" required />
+        <input
+          v-model="form.password"
+          type="password"
+          name="password"
+          placeholder="******"
+          class="forminput"
+          required
+        />
       </div>
     </div>
     <a href="#" class="button-secondary">Lupa Password</a>
-    <input type="submit" value="Login" class="button-primary" />
+    <input type="submit" value="Login" @click.prevent="onSubmit" class="button-primary" />
   </form>
   <!-- End Block Form -->
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: 'LoginForm',
   data() {
     return {
       title: 'Selamat Data',
-      inputemail: '',
-      inputpwd: '',
-      active: true,
-      old: 50,
+      urlLogin: 'http://localhost:8000/api/v1/login',
+      form: {
+        email: '',
+        password: '',
+      },
+      active: true
+    }
+  },
+  created() {
+    console.log("Created");
+  },
+  methods: {
+    async onSubmit() {
+      try {
+        // sukses
+        const response = await axios({
+          method: "post",
+          url: this.urlLogin,
+          data: this.form,
+        });
+        if(response.data.status == 'success') {
+          const access_token = response.data.access_token;
+          const expired_token = response.data.expires_in;
+
+          // save to localStorage
+          localStorage.setItem("access_token",access_token);
+          localStorage.setItem("expired_token",expired_token);
+
+          // redirect dashboard
+          console.log("Sukses Login "+access_token);
+        }
+      } catch (error) {
+        // error
+        console.log(error);
+      }
     }
   }
 }
